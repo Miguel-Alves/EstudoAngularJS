9e9ea4be241fb2668910dfc08fcc9f4d1891778c
Miguel Henrique de Oliveira Alves <miguel.alves@rede.stf.gov.br>
2020-02-18 19:25:19 -0300

    Desenvolvimento de lista telefônica para aprendizado de AngularJS. O projeto já se encontra um pouco avançado. Porém, ainda possui espaço para melhoras.

---
+++ js/controllers/listaTelefonicaCtrl.js
@@ -0,0 +1,56 @@
+angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($scope, contatosAPI, operadorasAPI, serialGenerator ){
+    $scope.app = "Lista Telefonica";
+    $scope.contatos = [];
+
+    $scope.operadoras = [];
+
+    var carregarContatos = function(){
+        contatosAPI.getContatos()
+        .then(function(response) {
+            $scope.contatos = response.data;
+        }, function(response) {
+            $scope.mesage= "Aconteceu um problema: " + response.data;
+        });
+    }
+
+    var carregarOperadoras = function(){
+        operadorasAPI.getOperadoras()
+        .then(function(response) {
+            $scope.operadoras= response.data;
+        }, function(response) {
+            $scope.mesage = "Aconteceu um problema: " + response.data;
+        });
+    }
+
+    $scope.adicionarContato = function(contato){
+        contato.serial = serialGenerator.generate();
+        contato.data = new Date();
+        contatosAPI.saveContato(contato)
+        .then(function() {
+            delete $scope.contato;
+            $scope.contatoForm.$setPristine();
+            carregarContatos();
+        }, function(response) {
+            $scope.mesage = "Aconteceu um problema: " + response.data;
+        });;
+    }
+
+    $scope.apagarContatos = function(contatos){
+        $scope.contatos =  contatos.filter(function(contato){
+            if (!contato.selecionado) return contato;
+        });
+    }
+
+    $scope.isContatoSelecionado = function(contatos){
+        return contatos.some(function(contato){
+            return contato.selecionado;
+        });
+        
+    }
+    $scope.ordenarPor = function(campo){
+        $scope.criterioDeOrdenacao = campo;
+        $scope.direcaoDeOrdenacao = !$scope.direcaoDeOrdenacao;
+    }
+    carregarContatos();
+    carregarOperadoras();
+});
---
+++ js/services/contatosAPIService.js
@@ -0,0 +1,15 @@
+angular.module("listaTelefonica").factory("contatosAPI", function($http, config){
+
+    var _getContatos = function() {
+        return $http.get(config.baseURL+"/contatos");
+    };
+    
+    var _saveContato = function (contato) {
+        return $http.post(config.baseURL+"/contatos", contato);
+    };
+
+    return {
+        getContatos: _getContatos,
+        saveContato: _saveContato
+    };
+});
